b86bec16124381b94b89c2e5352a889ebc0a1f4b
Soumya Sudipta Sarangi <soumyasudiptasarangi@gmail.com>
2020-09-11 01:53:03 +0530

    Added first commit
    Added StrapiAPI and a minimal web interface for events application.

---
+++ event-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64e8e533-086d-4234-a699-2268c9d9c04d'
+};
---
+++ event-front/src/layouts/Default.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>
+        <g-link to="/">Occasions</g-link>
+      </v-toolbar-title>
+
+       <v-text-field
+            v-model="searchText"
+            @click:clear="searchText = ''"
+            placeholder="Search"
+            class="ml-8"
+            style="max-width:350px"
+            prepend-inner-icon="mdi-magnify"
+            outlined
+            rounded
+            clearable
+            dense
+            hide-details
+        />
+
+      <v-spacer/>
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col sm="6" offset-sm="3">
+            <slot :searchText="searchText" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style scoped>
+.v-toolbar__title a {
+  text-decoration: none;
+  color: black;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText : ''
+    }
+  }
+}
+</script>
